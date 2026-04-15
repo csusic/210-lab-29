7cb4db31d3606eafa6921e846cd8c2523fb80e03
Christine Susic <csusic022@insite.4cd.edu>
2026-04-15 16:25:28 -0700

    Commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+// COMSC-210 | Lab 29 | Christine Susic
+
+
+//simulation structure
+//3-element std::array of std::lists
+
+//std::map
+
+
+
+
+//function 
+
+
+//main function
+
+	//declarations
+	//create map 
+   
+    //open file
+    //file not found
+    
+    //retrieve data from an external file
+    //at least 100 lines of data
+    //close file
+
+
+        
+//main function
